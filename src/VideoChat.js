139444ccd9a875102d2cf339a431b19d0524a399
David Kim <youup99@gmail.com>
2020-09-15 22:53:40 -0600

    Add synchronous room

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from "react";
 import Lobby from "./Lobby";
+import Room from "./Room";
 
 const VideoChat = () => {
   const [username, setUsername] = useState("");
@@ -39,11 +40,7 @@ const VideoChat = () => {
   let render;
   if (token) {
     render = (
-      <div>
-        <p>Username: {username}</p>
-        <p>Room Name: {roomName}</p>
-        <p>Token: {token}</p>
-      </div>
+      <Room roomName={roomName} token={token} handleLogout={handleLogout} />
     );
   } else {
     render = (
